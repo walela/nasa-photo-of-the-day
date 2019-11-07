9cf9287d4dfa6c2e09fb5eaa5a16c1159f0c4059
Austin Walela <austinmasibo@gmail.com>
2019-11-07 09:25:18 +0300

    added  data fetch from NASA api and logged res.data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "./App.css";
 
+const nasa_api =
+  "https://api.nasa.gov/planetary/apod?api_key=ZGHKUkdfKsBpmBKsIgwMQCURhmc8Pz5bV9rTgufI";
 function App() {
+  const [pod, setPod] = useState({});
+
+  useEffect(() => {
+    axios
+      .get(nasa_api)
+      .then(res => {
+        console.log(res.data);
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  }, []);
   return (
     <div className="App">
       <p>
